14ace155571cc4ff4dcbb61466f1ddfe90181eb5
Mike Kelly <pioto@pioto.org>
2012-05-23 09:28:09 -0400

    Fix compilation of smtp-dummy on FreeBSD

--- test/smtp-dummy.c
+++ test/smtp-dummy.c
@@ -33,11 +33,14 @@
  * have been warned.
  */
 
+#define _GNU_SOURCE /* for getline */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <netinet/ip.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
 
